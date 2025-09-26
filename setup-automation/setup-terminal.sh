774f7902866cc7432ba64969b3ada22f2cc080ed
Ryan Jarvinen <ryan.jarvinen@gmail.com>
2025-09-26 12:07:38 -0700

    vscode

--- setup-automation/setup-terminal.sh
+++ setup-automation/setup-terminal.sh
@@ -9,7 +9,56 @@ echo "Setup vm control01" > /tmp/progress.log
 chmod 666 /tmp/progress.log 
 
 #dnf install -y nc
+# Install VSCode
+curl -fsSL https://code-server.dev/install.sh | sh
 
+cat >/home/rhel/.config/code-server/config.yaml << EOF
+bind-addr: 0.0.0.0:9090
+auth: none
+cert: false
+disable-update-check: true
+EOF
+
+cat >/home/$USER/.local/share/code-server/User/settings.json <<EOL
+{
+  "git.ignoreLegacyWarning": true,
+  "window.menuBarVisibility": "visible",
+  "git.enableSmartCommit": true,
+  "workbench.tips.enabled": false,
+  "workbench.startupEditor": "readme",
+  "telemetry.enableTelemetry": false,
+  "search.smartCase": true,
+  "git.confirmSync": false,
+  "workbench.colorTheme": "Solarized Dark",
+  "update.showReleaseNotes": false,
+  "update.mode": "none",
+  "ansible.ansibleLint.enabled": false,
+  "ansible.ansible.useFullyQualifiedCollectionNames": true,
+  "files.associations": {
+      "*.yml": "ansible",
+      "*.yaml": "ansible"
+  },
+  "files.exclude": {
+    "**/.*": true
+  },
+  "files.watcherExclude": {
+    "**": true
+  },
+  "files.watcherExclude": {
+    "**": true
+  },
+  "security.workspace.trust.enabled": false,
+  "redhat.telemetry.enabled": false,
+  "ansibleLint.enabled": false,
+  "ansible.validation.lint.enabled": false,
+  "ansible.validation.enabled": false,
+  "ansible.lightspeed.enabled": false,
+  "ansible.lightspeed.suggestions.enabled": false
+  "python.useEnvironmentsExtension": false
+}
+EOL
+
+sudo systemctl enable --now code-server@$USER
 
 ##Imported init from Instruqt:
 export GRAALVM_VERSION=22.3.1
@@ -28,14 +77,7 @@ echo "export GRAALVM_HOME=/opt/java/graalvm-ce-java17-${GRAALVM_VERSION}/" >> /h
 echo 'export JAVA_HOME=$GRAALVM_HOME' >> /home/rhel/.bashrc
 echo "export MAVEN_HOME=/opt/java/apache-maven-${MVN_VERSION}/" >> /home/rhel/.bashrc
 echo 'export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"'  >> /home/rhel/.bashrc
-echo "export GRAALVM_HOME=/opt/java/graalvm-ce-java17-${GRAALVM_VERSION}/" >> /root/.bashrc
-echo 'export JAVA_HOME=$GRAALVM_HOME' >> /root/.bashrc
-echo "export MAVEN_HOME=/opt/java/apache-maven-${MVN_VERSION}/" >> /root/.bashrc
-echo 'export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"'  >> /root/.bashrc
 
 mkdir -p /home/rhel/projects/quarkus
-mkdir -p /root/projects/quarkus
-rm -Rf /root/projects/quarkus/getting-started
-echo "-w \"\n\"" >> /root/.curlrc
 echo "-w \"\n\"" >> /home/rhel/.curlrc
 chown -R rhel.rhel /home/rhel
